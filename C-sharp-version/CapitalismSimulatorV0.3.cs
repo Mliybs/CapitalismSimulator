2b02f30b86001a57c46774e87d50e983fe481b61
Mliybs <2491988463@qq.com>
2022-11-12 23:35:19 +0800

    test

--- C-sharp-version/CapitalismSimulatorV0.3.cs
+++ C-sharp-version/CapitalismSimulatorV0.3.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
+public class backup
+{
+    public List<int> list = new List<int>(){1,2};
+    public int A = 1;
+}
 class main
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("TEST");
+        backup back = new backup();
+        Console.WriteLine(back.A);
     }
 }
