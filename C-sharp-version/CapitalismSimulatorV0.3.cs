150d978bef207cd198e5f1e4c26cd592ae19abb1
Mliybs <2491988463@qq.com>
2022-11-17 00:22:13 +0800

    update

--- C-sharp-version/CapitalismSimulatorV0.3.cs
+++ C-sharp-version/CapitalismSimulatorV0.3.cs
@@ -4,23 +4,83 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-class backup
+class backup //主要参数类
 {
     public bool pause = false;
+
     public int round = 0;
+
     public int temp = 0;
+
     public int number = 0;
+
     public int item = 0;
+
     public int G = 1000;
+    
     public int Pm = 0;
+
     public int A = 0;
-    public int[] list;
-    public string[] listing;
+
+    public List<List<int>> list = new List<List<int>>();
+
+    public List<string> listing = new List<string>();
 }
-class main
+class main //程序主进程
 {
     public static void Main(string[] args)
     {
-        backup back = new backup();
+        T1 T1;
+
+        T2 T2;
+
+        backup backup = new backup();
+
+        //初始化
+
+        while (backup.pause == false)
+        {
+            backup.temp = 0;
+
+            backup.number = 0;
+
+            backup.item = 0;
+
+            backup.round += 1;
+
+            //缓存重置
+        }
     }
+}
+struct T1 //木板相关数值结构
+{
+    public const int code = 1;
+
+    public const string name = "木板";
+
+    public int number;
+
+    public const int Pm = 16;
+
+    public const int A = 4;
+
+    public const int m = 4;
+
+    public const int W = 24;
+}
+struct T2 //螺丝相关数值结构
+{
+    public const int code = 2;
+
+    public const string name = "螺丝";
+
+    public int number;
+
+    public const int Pm = 2;
+
+    public const int A = 2;
+
+    public const int m = 2;
+
+    public const int W = 6;
 }
